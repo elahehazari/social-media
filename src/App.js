6ef54f1b2b0468dd6c44ceb9b6812bc5729fba4b
Elina Alizadeh <elinaalizadeh68@yahoo.com>
2021-07-21 18:10:57 -0700

    add css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
+import "./App.css";
+
 function App() {
   return (
-    <div className="mainPage">
-      <div className="header">
-        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"></img>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          minlength="4"
-          maxlength="8"
-          size="10"
-        ></input>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          minlength="4"
-          maxlength="8"
-          size="10"
-        ></input>
-      </div>
+    <div className="container">
+      <img
+        className="picture"
+        alt="profile"
+        src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      ></img>
+      <h3 className="user-name">Login</h3>
+      <p className="bio">text</p>
     </div>
   );
 }
